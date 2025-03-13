be81dda22993ab951bd99e519bfeef09c5bf80d0
anyex-net <94890879+anyex-net@users.noreply.github.com>
2025-03-13 09:30:38 +0800

    add viabtcex server

---
+++ apps-service/apps-service-viabtcex/apps-service-viabtcex-tradehistory/src/main/java/com/anyex/apps/AppsRPCServerViabtcexTradehistory.java
@@ -0,0 +1,59 @@
+package com.anyex.apps;
+
+import com.alipay.sofa.rpc.common.RpcOptions;
+import com.anyex.apps.config.GlobalProperies;
+import com.anyex.apps.redis.RedisProperties;
+import com.anyex.apps.utils.JpushUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@Configuration
+@EnableCaching
+@EnableAsync
+@EnableTransactionManagement
+@ComponentScan("com.anyex")
+@ImportResource({"classpath*:sofa-provider.xml"})
+@EnableConfigurationProperties(RedisProperties.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class, MongoAutoConfiguration.class})
+@EnableMongoRepositories(basePackages = "com.anyex.apps.config")
+@MapperScan(basePackages = {"com.anyex.apps.**.mapper"})
+public class AppsRPCServerViabtcexTradehistory
+{
+    public static void main(String[] args)
+    {
+        // 禁用链路跟踪日志
+        System.setProperty(RpcOptions.DEFAULT_TRACER, "");
+        SpringApplication springApplication = new SpringApplication(AppsRPCServerViabtcexTradehistory.class);
+        springApplication.setBannerMode(Banner.Mode.CONSOLE);
+        springApplication.run(args);
+    }
+    
+    /**
+     * 极光推送服务
+     * @param properies
+     * @return
+     */
+    @Bean
+    public JpushUtils jpushUtils(GlobalProperies properies)
+    {
+        JpushUtils jpush = new JpushUtils();
+        jpush.setProperies(properies);
+        return jpush;
+    }
+}
---
+++ apps-service/apps-service-viabtcex/apps-service-viabtcex-tradelog/src/test/java/com/anyex/apps/BaseServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.anyex.apps;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * BaseServiceImplTest
+ * <p>File: BaseServiceImplTest.java </p>
+ * <p>Title: BaseServiceImplTest </p>
+ * <p>Description: BaseServiceImplTest </p>
+ * <p>Copyright: Copyright (c) 2019-02-01</p>
+ * <p>Company: AnyEx</p>
+ *
+ * @author Playguy
+ * @version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {AppsRPCServerViabtcexTradelog.class})
+public class BaseServiceImplTest
+{
+}
