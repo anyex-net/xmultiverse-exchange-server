bc1eeff385c7e705d89d655b2c3665f5baae7e14
anyex-net <94890879+anyex-net@users.noreply.github.com>
2025-03-20 17:29:23 +0800

    fix user login & forgetPass

--- apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/user/ForgetPassController.java
+++ apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/user/ForgetPassController.java
@@ -64,7 +64,7 @@ public class ForgetPassController extends GenericController
     @ResponseBody
     @RequestMapping("/forgetPass/submit")
     @ApiOperation(value = "1确认用户是否存在", httpMethod = "POST")
-    @AccessLimit(limit = 1, timeScope = 60, isLogin = false) // 未登录情况下限制60秒内最多请求1次
+    @AccessLimit(limit = 1, timeScope = 30, isLogin = false) // 未登录情况下限制30秒内最多请求1次
     public JsonMessage<RespUserForgetPass> submit(HttpServletRequest request, @Validated @RequestBody ReqUserForgetPass reqUserForgetPass /*@ModelAttribute AliyunModel model*/) throws BusinessException
     {
         log.info("submit reqUserForgetPass:{}", reqUserForgetPass);
