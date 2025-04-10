7a19b458d97a6fa087991a1157838224e182bbc7
tomson8710 <463142815@qq.com>
2025-04-10 20:40:29 +0800

    add RaiseMargin

--- apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/rwa/RwaInstSpvProductController.java
+++ apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/rwa/RwaInstSpvProductController.java
@@ -6,28 +6,29 @@ package com.anyex.apps.controller.rwa;
 
 import com.anyex.apps.bean.GenericController;
 import com.anyex.apps.controller.rwa.req.ReqRwaInstSpvProduct;
+import com.anyex.apps.controller.rwa.req.ReqRwaInstSpvProductDividend;
+import com.anyex.apps.controller.rwa.req.ReqRwaInstSpvProductDividendPagination;
 import com.anyex.apps.controller.rwa.req.ReqRwaInstSpvProductPagination;
 import com.anyex.apps.controller.rwa.resp.RespRwaInstSpvProduct;
 import com.anyex.apps.enums.CommonEnums;
 import com.anyex.apps.exception.BusinessException;
 import com.anyex.apps.model.JsonMessage;
 import com.anyex.apps.model.PaginateResult;
-import com.anyex.apps.rwa.entity.RwaCertInstSpvPromoter;
-import com.anyex.apps.rwa.entity.RwaInstSpvCompany;
-import com.anyex.apps.rwa.entity.RwaInstSpvProduct;
-import com.anyex.apps.rwa.service.RwaCertInstSpvPromoterService;
-import com.anyex.apps.rwa.service.RwaInstSpvCompanyService;
-import com.anyex.apps.rwa.service.RwaInstSpvProductService;
+import com.anyex.apps.rwa.entity.*;
+import com.anyex.apps.rwa.service.*;
 import com.anyex.apps.shiro.model.UserPrincipal;
 import com.anyex.apps.utils.OnLineUserUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hpsf.Decimal;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 /**
  * RWA机构SPV产品 控制器
@@ -53,6 +54,12 @@ public class RwaInstSpvProductController extends GenericController
 
     @Autowired(required = false)
     private RwaInstSpvCompanyService rwaInstSpvCompanyService;
+
+    @Autowired(required = false)
+    private RwaInstSpvProductDividendService rwaInstSpvProductDividendService;
+
+    @Autowired(required = false)
+    private RwaCertInstInvestorService rwaCertInstInvestorService;
 
     @GetMapping(value = "/getRwaInstSpvProduct")
     @ApiOperation(value = "获取RWA机构SPV产品详情", httpMethod = "GET")
@@ -120,5 +127,73 @@ public class RwaInstSpvProductController extends GenericController
         rwaInstSpvProduct.setUserId(principal.getId());
         PaginateResult<RwaInstSpvProduct> result = rwaInstSpvProductService.search(pagin,rwaInstSpvProduct);
         return getJsonMessage(CommonEnums.SUCCESS, result);
+    }
+
+    @PostMapping(value = "/submitRaiseMargin")
+    @ApiOperation(value = "提交保证金", httpMethod = "POST")
+    public JsonMessage submitRaiseMargin(Long id) throws BusinessException
+    {
+        UserPrincipal principal = OnLineUserUtils.getPrincipal();
+        if (null == principal) throw new BusinessException(CommonEnums.USER_NOT_LOGIN);
+
+        JsonMessage json = getJsonMessage(CommonEnums.SUCCESS);
+        if (null == id) throw new BusinessException(CommonEnums.ERROR_PARAMS_VALID);
+        RwaInstSpvProduct rwaInstSpvProduct = rwaInstSpvProductService.selectByPrimaryKey(id);
+        if (null == rwaInstSpvProduct)
+            throw new BusinessException(CommonEnums.ERROR_DATA_NO_FOUND_ERR);
+        //需转保证金,需调划转接口
+        BigDecimal raiseMargin = rwaInstSpvProduct.getRaiseMarginRatio().multiply(rwaInstSpvProduct.getRaiseAmount());
+
+
+        //
+        rwaInstSpvProduct.setRaiseMarginState(1);
+        rwaInstSpvProductService.updateByPrimaryKeySelective(rwaInstSpvProduct);
+        return json;
+    }
+
+    @PostMapping(value = "/RwaInstSpvProductDividendData")
+    @ApiOperation(value = "查询RWA机构SPV产品分红管理列表", httpMethod = "POST")
+    public JsonMessage<PaginateResult<RwaInstSpvProductDividend>> RwaInstSpvProductDividendData(@Validated @RequestBody ReqRwaInstSpvProductDividendPagination pagin) throws BusinessException
+    {
+        UserPrincipal principal = OnLineUserUtils.getPrincipal();
+        if (null == principal) throw new BusinessException(CommonEnums.USER_NOT_LOGIN);
+
+        RwaInstSpvProductDividend rwaInstSpvProductDividend = new RwaInstSpvProductDividend();
+        BeanUtils.copyProperties(pagin, rwaInstSpvProductDividend);
+        rwaInstSpvProductDividend.setUserId(principal.getId());
+        PaginateResult<RwaInstSpvProductDividend> result = rwaInstSpvProductDividendService.search(pagin,rwaInstSpvProductDividend);
+        return getJsonMessage(CommonEnums.SUCCESS, result);
+    }
+
+    @PostMapping(value = "/submitRwaInstSpvProductDividend")
+    @ApiOperation(value = "执行分红单", httpMethod = "POST")
+    public JsonMessage submitRwaInstSpvProductDividend(@Validated @RequestBody ReqRwaInstSpvProductDividend reqrwaInstSpvProductDividend) throws BusinessException {
+        UserPrincipal principal = OnLineUserUtils.getPrincipal();
+        if (null == principal) throw new BusinessException(CommonEnums.USER_NOT_LOGIN);
+
+        JsonMessage json = getJsonMessage(CommonEnums.SUCCESS);
+        if (beanValidator(json, reqrwaInstSpvProductDividend)) {
+            RwaInstSpvProductDividend rwaInstSpvProductDividend = new RwaInstSpvProductDividend();
+            BeanUtils.copyProperties(reqrwaInstSpvProductDividend, rwaInstSpvProductDividend);
+            //
+            if (null == rwaInstSpvProductDividend.getId()) {
+                rwaInstSpvProductDividend.setCreateTime(System.currentTimeMillis());
+                rwaInstSpvProductDividend.setUserId(principal.getId());
+            }
+//            rwaInstSpvProductDividend.setUpdateTime(System.currentTimeMillis());
+            rwaInstSpvProductDividend.setState("pending");
+            RwaCertInstInvestor rwaCertInstInvestor = new RwaCertInstInvestor();
+            rwaCertInstInvestor.setUserId(principal.getId());
+            RwaCertInstInvestor rwaCertInstInvestor1 = rwaCertInstInvestorService.selectOne(rwaCertInstInvestor);
+            rwaInstSpvProductDividend.setInstInvestorId(rwaCertInstInvestor1.getId());
+            //
+            log.info("entity:{}", rwaInstSpvProductDividend);
+            if (null == rwaInstSpvProductDividend.getId()) {
+                rwaInstSpvProductDividendService.insert(rwaInstSpvProductDividend);
+            } else {
+                rwaInstSpvProductDividendService.updateByPrimaryKeySelective(rwaInstSpvProductDividend);
+            }
+        }
+        return json;
     }
 }
---
+++ apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/rwa/req/ReqRwaInstSpvProductDividendPagination.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 Apps, Inc. All rights reserved. com.anyex
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.anyex.apps.controller.rwa.req;
+
+import com.anyex.apps.model.Pagination;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * RWA机构SPV产品分红记录 分页请求对象
+ * <p>File：ReqRwaInstSpvProductDividend.java</p>
+ * <p>Title: ReqRwaInstSpvProductDividend</p>
+ * <p>Description:ReqRwaInstSpvProductDividend</p>
+ * <p>Copyright: Copyright (c) May 26, 2015</p>
+ * <p>Company: AnyEx</p>
+ * @author Playguy
+ * @version 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(description = "RWA机构SPV产品分红记录分页请求对象")
+public class ReqRwaInstSpvProductDividendPagination extends Pagination
+{
+	private static final long serialVersionUID = 1L;
+	
+//	/**用户ID*/
+//	@ApiModelProperty(value = "用户ID", position = 1)
+//	private Long userId;
+//
+//	/**机构投资者ID*/
+//	@ApiModelProperty(value = "机构投资者ID", position = 2)
+//	private Long instInvestorId;
+
+	/**机构SPV产品ID*/
+	@ApiModelProperty(value = "机构SPV产品ID", position = 3)
+	private Long instSpvProductId;
+
+	/**分红币种*/
+	@ApiModelProperty(value = "分红币种", position = 4)
+	private String dividendCurrency;
+
+	/**分红金额*/
+	@ApiModelProperty(value = "分红金额", position = 5)
+	private java.math.BigDecimal dividendAmount;
+
+	/**状态(成功success、处理中pending、失败failed)*/
+	@ApiModelProperty(value = "状态(成功success、处理中pending、失败failed)", position = 6)
+	private String state;
+
+	/**备注*/
+	@ApiModelProperty(value = "备注", position = 7)
+	private String remark;
+
+//	/**创建时间*/
+//	@ApiModelProperty(value = "创建时间", position = 8)
+//	private java.lang.Long createTime;
+//
+//	/**更新人*/
+//	@ApiModelProperty(value = "更新人", position = 9)
+//	private java.lang.String updateBy;
+//
+//	/**更新时间*/
+//	@ApiModelProperty(value = "更新时间", position = 10)
+//	private java.lang.Long updateTime;
+}
+
