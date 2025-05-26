a5253713b8ebb5212214868cf701252f692d502c
anyex-net <94890879+anyex-net@users.noreply.github.com>
2025-05-26 19:45:10 +0800

    fix RwaInstSpvProductDividend quartz

--- apps-backend/apps-backend-quartz/src/main/java/com/anyex/apps/task/fund/WithdrawalHistoryTask.java
+++ apps-backend/apps-backend-quartz/src/main/java/com/anyex/apps/task/fund/WithdrawalHistoryTask.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.anyex.apps.fund.entity.WithdrawalHistory;
 import com.anyex.apps.fund.service.WithdrawalHistoryService;
+import com.anyex.apps.rwa.entity.RwaInstSpvProductDividend;
+import com.anyex.apps.rwa.service.RwaInstSpvProductDividendService;
 import com.anyex.apps.user.entity.User;
 import com.anyex.apps.user.service.UserService;
 import com.anyex.apps.utils.RedisLock;
@@ -32,6 +34,9 @@ public class WithdrawalHistoryTask
     @Autowired(required = false)
     private WithdrawalHistoryService withdrawalHistoryService;
 
+    @Autowired(required = false)
+    private RwaInstSpvProductDividendService rwaInstSpvProductDividendService;
+
     /**
      * 提现历史调度
      * @throws RuntimeException
@@ -56,12 +61,16 @@ public class WithdrawalHistoryTask
                     JSONArray jsonArray = jsonObjectResp.getJSONArray("data");
                     if(null != jsonArray && jsonArray.size() > 0)
                     {
+                        //
                         WithdrawalHistory withdrawalHistorySearch = new WithdrawalHistory();
+                        RwaInstSpvProductDividend rwaInstSpvProductDividendSearch = new RwaInstSpvProductDividend();
                         User userSearch = new User();
+                        //
                         for(int i=0; i<jsonArray.size(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
                             log.info("get_tx_list jsonArray index {} : {}", i+1, jsonObject);
                             // get_tx_list jsonArray index 1 : {"tx_status":"completed","coin_no":"b5c70cd1-5bdc-4783-beba-f515bd3581ad","amount":"1.100000","request_no":"0f61a212-86ff-4e2a-a543-4a0c796608bd","chain_code":"ETH","tx_hash":"0xb1e9ee372e44124a806bb0a84450d2d306f7c8474e9b854446a654ac8458b9e5","from":"","to":"0xDb6B16F3381CC3482bE7ca2EDCC465d0c0aCD6e1","tx_type":"WITHDRAW","coin_code":"USDTF","user_no":"85d94e47-157d-4f57-bd60-ce07d9b6ac35"}
+                            //
                             withdrawalHistorySearch.setTransId(jsonObject.getString("request_no"));
                             WithdrawalHistory withdrawalHistoryDB = withdrawalHistoryService.selectOne(withdrawalHistorySearch);
                             if(null != withdrawalHistoryDB){
@@ -89,6 +98,32 @@ public class WithdrawalHistoryTask
                                 }
                             } else {
                                 log.error("对应的提现记录不存在，无需执行！");
+                            }
+                            //
+                            //
+                            rwaInstSpvProductDividendSearch.setRemark(jsonObject.getString("request_no"));
+                            RwaInstSpvProductDividend rwaInstSpvProductDividendDB = rwaInstSpvProductDividendService.selectOne(rwaInstSpvProductDividendSearch);
+                            if(null != rwaInstSpvProductDividendDB){
+                                userSearch.setRemark(jsonObject.getString("user_no"));
+                                User userDB = userService.selectOne(userSearch);
+                                if(null != userDB){
+                                    log.info("userDB:{}", userDB);
+                                    //
+                                    // 推送交易状态
+                                    jsonObjectResp = XMWalletApi.push_tx_status(jsonObject.getString("request_no"));
+                                    log.info("push_tx_status jsonObjectResp:{}", jsonObjectResp);
+                                    // push_tx_status jsonObjectResp:{"code":0,"signature":"ea19a979f4afcaafafef882a7a6e546ef6f8947565550437d2644c819ae47cb3","message":"Success"}
+                                    if(null != jsonObjectResp && "0".equals(jsonObjectResp.getString("code"))){
+                                        JSONObject jsonObjectData = jsonObjectResp.getJSONObject("data");
+                                        log.info("push_tx_status jsonObjectData data: {}", jsonObjectData);
+                                    } else {
+                                        log.error("push_tx_status jsonObjectResp data: {}", jsonObjectResp);
+                                    }
+                                } else {
+                                    log.error("对应的用户不存在，暂时无法执行，请检查！");
+                                }
+                            } else {
+                                log.error("对应的RWA机构SPV产品分红记录记录不存在，无需执行！");
                             }
                         }
                     }
