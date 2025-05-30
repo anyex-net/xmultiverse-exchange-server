0741b24e2f61586b6e34b409663e354d7d419cb3
tomson8710 <463142815@qq.com>
2025-05-30 13:40:06 +0800

    fix

--- apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/rwa/RwaInstSpvProductController.java
+++ apps-fronts/apps-fronts-web/src/main/java/com/anyex/apps/controller/rwa/RwaInstSpvProductController.java
@@ -337,8 +337,8 @@ public class RwaInstSpvProductController extends GenericController
         if (beanValidator(json, reqrwaInstSpvProductDividend)) {
             RwaInstSpvProductDividend rwaInstSpvProductDividend = new RwaInstSpvProductDividend();
             BeanUtils.copyProperties(reqrwaInstSpvProductDividend, rwaInstSpvProductDividend);
-            RwaInstSpvProductDividend rwaInstSpvProductDividendDB = rwaInstSpvProductDividendService.selectOne(rwaInstSpvProductDividend);
-            if ("5".equals(rwaInstSpvProductDividendDB.getState())){
+            RwaInstSpvProduct rwaInstSpvProduct = rwaInstSpvProductService.selectByPrimaryKey(rwaInstSpvProductDividend.getInstSpvProductId());
+            if ("5".equals(rwaInstSpvProduct.getState())){
                 log.error("还在申购阶段，不能进行分红");
                 throw new BusinessException(CommonEnums.ERROR_RWA_PROUDCT_ISSUE_NOT_START);
             }
