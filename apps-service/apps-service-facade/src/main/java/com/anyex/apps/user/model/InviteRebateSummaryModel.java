9b7d9d73a6e5aebd140a1e84a3e9008bec238811
tomson8710 <463142815@qq.com>
2025-06-03 09:05:03 +0800

    fix

--- apps-service/apps-service-facade/src/main/java/com/anyex/apps/user/model/InviteRebateSummaryModel.java
+++ apps-service/apps-service-facade/src/main/java/com/anyex/apps/user/model/InviteRebateSummaryModel.java
@@ -1,6 +1,7 @@
 package com.anyex.apps.user.model;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 
@@ -11,7 +12,12 @@ import java.math.BigDecimal;
 @ApiModel("邀请统计")
 public class InviteRebateSummaryModel implements Serializable {
 
+    @ApiModelProperty(value = "邀请人数")
     private Integer totalInvites;   // 邀请人数
+
+    @ApiModelProperty(value = "交易人数")
     private Integer tradedUsers;    // 交易人数
+
+    @ApiModelProperty(value = "累计返佣金额")
     private BigDecimal totalRebate; // 累计返佣金额
 }
--- apps-service/apps-service-facade/src/main/java/com/anyex/apps/user/model/UserInviteRebateModel.java
+++ apps-service/apps-service-facade/src/main/java/com/anyex/apps/user/model/UserInviteRebateModel.java
@@ -1,6 +1,7 @@
 package com.anyex.apps.user.model;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,13 +12,18 @@ import java.math.BigDecimal;
 @ApiModel("邀请用户分别统计")
 public class UserInviteRebateModel implements Serializable {
 
+    @ApiModelProperty(value = "被邀请者ID")
     private Long inviteeId;
 
+    @ApiModelProperty(value = "用户UID")
     private Long uid;
 
+    @ApiModelProperty(value = "是否有效")
     private Integer isValid;
 
+    @ApiModelProperty(value = "累计返佣")
     private BigDecimal totalRebateAmount;
 
+    @ApiModelProperty(value = "注册时间")
     private Long createTime;
 }
